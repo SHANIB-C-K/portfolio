1be6c009abd06c4ec013d96ef1bb1bec699f74cd
SHANIB C K <ckshanib8@gmail.com>
2024-04-11 12:27:59 +0530

    links created successfully

---
+++ script.js
@@ -0,0 +1,29 @@
+function Home() {
+  document.getElementById("Home").classList.add("active");
+  document.getElementById("About").classList.remove("active");
+  document.getElementById("Services").classList.remove("active");
+  document.getElementById("Contact").classList.remove("active");
+}
+
+function About() {
+  document.getElementById("About").classList.add("active");
+  document.getElementById("Home").classList.remove("active");
+  document.getElementById("Services").classList.remove("active");
+  document.getElementById("Contact").classList.remove("active");
+}
+
+function Services() {
+  document.getElementById("Services").classList.add("active");
+  document.getElementById("Home").classList.remove("active");
+  document.getElementById("About").classList.remove("active");
+  document.getElementById("Services").classList.remove("active");
+  document.getElementById("Contact").classList.remove("active");
+}
+
+function Contact() {
+  document.getElementById("Contact").classList.add("active");
+  document.getElementById("Home").classList.remove("active");
+  document.getElementById("About").classList.remove("active");
+  document.getElementById("Services").classList.remove("active");
+  document.getElementById("Contact").classList.remove("active");
+}
